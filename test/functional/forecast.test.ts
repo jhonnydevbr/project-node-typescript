9dbdf4660e90fa90e1b9718bcded522b9f84f769
jhonnydevbr <jhonnybrustolin@gmail.com>
2024-04-15 20:44:19 -0300

    introduzindo forecast

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,4 +1,4 @@
-import supertest from 'supertest';
+import supertest from "supertest";
 
 describe('Beach forecast functional tests', () => {
   it('should return a forecast with just a few times', async() => {
